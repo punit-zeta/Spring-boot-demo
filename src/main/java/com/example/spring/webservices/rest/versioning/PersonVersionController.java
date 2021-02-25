44bb112c12b377a10bd8e1149986182ed304fb3f
punit-zeta <punits@zeta.tech>
2021-02-25 16:52:12 +0530

    Implemented Basic Authentication with Spring Security

--- src/main/java/com/example/spring/webservices/rest/versioning/PersonVersionController.java
+++ src/main/java/com/example/spring/webservices/rest/versioning/PersonVersionController.java
@@ -3,9 +3,21 @@ package com.example.spring.webservices.rest.versioning;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/*
+ * Factors:
+ * 1. URI pollution
+ * 2. Misuse of HTTP headers
+ * 3. Caching
+ * 4. Can we execute from browser
+ * 5. API documentation
+ * 
+ * No perfect solution
+ */
+
 @RestController
 public class PersonVersionController {
 	
+	// used in Twitter
 	@GetMapping("v1/person")
 	public PersonV1 personV1() {
 		return new PersonV1("Bob Charlie");
@@ -16,6 +28,7 @@ public class PersonVersionController {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
 
+	// used in Amazon
 	@GetMapping(value = "/person/param", params = "version=1")
 	public PersonV1 paramV1() {
 		return new PersonV1("Bob Charlie");
@@ -26,11 +39,14 @@ public class PersonVersionController {
 		return new PersonV2(new Name("Bob", "Charlie"));
 	}
 
+	// used in Microsoft
 	@GetMapping(value = "/person/header", headers = "X-API-VERSION=1")
 	public PersonV1 headerV1() {
 		return new PersonV1("Bob Charlie");
 	}
-
+	
+	
+    // used in GitHub
 	@GetMapping(value = "/person/header", headers = "X-API-VERSION=2")
 	public PersonV2 headerV2() {
 		return new PersonV2(new Name("Bob", "Charlie"));
